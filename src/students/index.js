eecaa46cca3b3bee46645264f44a3531b2227571
Federico Soncini <federicosoncini@Federicos-Air.fritz.box>
2020-12-08 17:05:24 +0100

    cleanup

--- src/students/index.js
+++ src/students/index.js
@@ -20,7 +20,6 @@ router.get("/", (req, res) => {
 //GET single student
 
 router.get("/:id", (req, res) => {
-  //   response.send("hello single user route")
   const studentsPathFile = path.join(__dirname, "students.json")
   const fileAsBuffer = fs.readFileSync(studentsPathFile)
   const file = fileAsBuffer.toString()
@@ -33,7 +32,6 @@ router.get("/:id", (req, res) => {
   )
   console.log("USER ", student)
 
-  // 4. send the found user back as a response
   res.send(student)
 })
 
@@ -43,13 +41,13 @@ router.post("/", (req, res) => {
   const fileAsBuffer = fs.readFileSync(studentsPathFile)
   const file = fileAsBuffer.toString()
   const studentsArray = JSON.parse(file)
-  //push new user to array
+
   const newStudent = req.body
   console.log(newStudent)
   newStudent.ID = uniqid()
 
   studentsArray.push(newStudent)
-  //replace old content with new
+
   fs.writeFileSync(studentsPathFile, JSON.stringify(studentsArray))
   res.status(201).send({id: newStudent.ID})
 })
@@ -61,19 +59,14 @@ router.put("/:id", (req, res) => {
   const fileAsAString = fileAsABuffer.toString()
   const studentsArray = JSON.parse(fileAsAString)
 
-  // 2. Filter out the user with the specified id
   const newStudentsArray = studentsArray.filter(
     (student) => student.ID !== req.params.id
   )
 
-  // 3. Create the modifiedUser object taking data from the request body
   const modifiedStudent = req.body
   modifiedStudent.ID = req.params.id
 
-  // 4. Add the modified user back to the array
   newStudentsArray.push(modifiedStudent)
-
-  // 5. Write it back on disk
 
   fs.writeFileSync(studentsFilePath, JSON.stringify(newStudentsArray))
   res.send("Modify user route")
@@ -82,18 +75,14 @@ router.put("/:id", (req, res) => {
 //DELETE
 
 router.delete("/:id", (req, res) => {
-  // 1. Read the file
   const studentsFilePath = path.join(__dirname, "students.json")
   const fileAsABuffer = fs.readFileSync(studentsFilePath)
   const fileAsAString = fileAsABuffer.toString()
   const studentsArray = JSON.parse(fileAsAString)
 
-  // 2. Filter out the user with the specified id
   const newStudentsArray = studentsArray.filter(
     (student) => student.ID !== req.params.id
   )
-
-  // 3. Save it back on disk
 
   fs.writeFileSync(studentsFilePath, JSON.stringify(newStudentsArray))
 
