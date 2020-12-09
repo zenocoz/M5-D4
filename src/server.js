ef38163780d74f99115643c196a039c18b3fb354
Federico Soncini <federicosoncini@Federicos-Air.fritz.box>
2020-12-09 08:32:34 +0100

    installed cors

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 const express = require("express")
 const studentsRoutes = require("./students")
+const cors = require("cors")
 
 const server = express()
 const port = 3001
 
+server.use(cors())
 server.use(express.json())
+
 server.use("/students", studentsRoutes)
 server.listen(port, () => console.log("server is running on port:", port))
